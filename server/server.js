48a198288f1dd6561139f27aabedf1618e42fc57
Seth Lewis <lewiscatenary@gmail.com>
2019-10-21 16:52:11 -0500

    proxy complete

--- server/server.js
+++ server/server.js
@@ -1,43 +1,18 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const path = require("path");
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-const db = require("../database/db")
+app.use(express.static(path.join(__dirname + "/../public/")));
 
 const PORT = 3001;
 
-app.get("/allproducts", (req, res) => {
-  db.findAllProducts()
-  .then(product => res.send(product))
-  .catch((err) => {
-    res.status(400).send("couldnt return those records")
-  })
-})
-
-app.get("/moreshopproducts", (req, res) => {
-  db.findShopProducts()
-  .then(product => res.send(product))
-  .catch((err) => {
-    res.status(400).send("couldnt return those records")
-  })
-})
-
-app.get("/product/:listing_id", (req, res) => {
-  const listingID = req.params
-  db.findCombinedProductsforID(listingID)
-  .then(product => res.send(product))
-  .catch((err) => {
-    res.status(400).send("couldnt find that record")
-  })
-})
-
-app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
